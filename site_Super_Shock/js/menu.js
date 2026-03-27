86d2194f298a8f91d38ca237b46d53c2a34f0b1a
Paola191107 <paola.farias.turmena@gmail.com>
2026-03-27 07:31:54 -0300

    agora esta certo

---
+++ site_Super_Shock/js/menu.js
@@ -0,0 +1,14 @@
+function showMenu() {
+    //alert ("clicou em mim!");
+    const menuLateral = document.getElementById('menu-lateral');
+    const iconMenu = document.getElementById("img-Menu");
+
+    menuLateral.classList.toggle("ativa");
+
+    if (menuLateral.classList.contains("ativa")) {
+
+        iconMenu.src = "img/icon-close-menu.png";
+    } else {
+        iconMenu.src = "img/icon-hamburger-menu.png";
+    }
+}
